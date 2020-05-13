2f6c8a6e3b2c4ac52a85a2c7ee1c344f1746e22b
Dhanny <dannysark16@gmail.com>
2020-05-13 08:44:34 +0000

    first commit

---
+++ first-code.js
@@ -0,0 +1,27 @@
+var container = 'Daniel';
+
+// variables
+
+// Data types
+
+// Comparisons
+
+// Basic operators, maths
+
+// Conditional operators: if
+
+// Logical operators
+
+// Nullish coalescing operator '??'
+
+// Loops: while and for
+
+// The "switch" statement
+
+// Functions
+
+// Function expressions
+
+// Arrow functions, the basics
+
+// JavaScript specials
